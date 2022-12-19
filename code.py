760f854d8b18ee3103c8822ab5f572aef0ebc805
Frankie Fox Wagoosh <frank.fox-wagoosh@stu.ocsb.ca>
2022-12-19 18:33:22 +0000

    added code.py

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Created By: Frankie Fox Wagoosh
+# Date: Dec , 19 , 2022
+# This program is the game that can be played on the PyBadge "Xenon"
+
+import ugame
+import stage
+
+def game_scene():
+    # This function is basically the game main theme
+
+    print("\n\n\n") # 3 blank lines to represent space between them
+    print("Hello , People we're in space!")
+
+    #This will repeat forever since it is a loop
+    while True:
+      pass # This is our placeholder for now 
+
+if __name__ == "_main_":
+       game_scene()
+
