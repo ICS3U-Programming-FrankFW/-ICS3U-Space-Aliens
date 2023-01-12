ca353a0e8415d07d77543e844dffaca9760f4a7a
Frankie Fox Wagoosh <frank.fox-wagoosh@stu.ocsb.ca>
2023-01-12 18:36:36 +0000

    added code.py

--- code.py
+++ code.py
@@ -1,76 +1,96 @@
-#!/usr/bin/env python3
-# Created By: Frankie Fox Wagoosh
-# Date: Dec , 19 , 2022
-# This program is the game that can be played on the PyBadge "Xenon"
 
+#!/usr/bin/env python3
+ 
+# Created By: Frankie
+# Date: Jan. 17th, 2023
 import ugame
 import stage
-
+ 
+import constants
+ 
+ 
 def game_scene():
-  # This function is basically the game main theme
-  
-  #The image banks for the circuit python 
-  image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
-  image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")    
-  
-  
-  #Set the background to image 0 in image bank
-  # and the size (10x8 tiles 16x16)
-  background = stage.Grid(image_bank_background, 10, 8)
-
-  # A sprite that will be updated every frame
-  ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    # this is the main scene for space alien
  
- # This makes it so it runs at an amazing 60 frames per second
- #also letting it create a stage for the background to show up
-  game = stage.Stage(ugame.display, 60)
-  
-  # Sets the layers , items show up in order 
+    #buttons you want to keep information on
+    a_button = constants.button_state["button_up"]
+    b_button = constants.button_state["button_up"]
+    start_button = constants.button_state["button_up"]
+    select_button = constants.button_state["button_up"]
  
-  game.layers = [ship] + [background]
-   
-   # Render the background and initial location of sprite list
-  #most likely you will only render background once per scene 
-  game.render_block()
+    #Gets the sound being able to play
+    pew_sound = open("pew.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
  
- #This will repeat forever since it is a loop
-  while True:
-   #get user input 
-   keys = ugame.buttons.get_pressed()
-
-   # This occurs when A is being pressed
-   if keys & ugame.K_X:
-    print("A")
-    # This occurs when B is being pressed
-   if keys & ugame.K_O:
-    print("B")
-   # This occurs when Start is being pressed
-   if keys & ugame.K_START:
-    print("Start")
-   # This occurs when Select is being pressed
-   if keys & ugame.K_SELECT:
-    print("Select") 
-   # This occurs when the right d pad is clicked moving the sprite to the right
-   if keys & ugame.K_RIGHT:
-    ship.move(ship.x + 1, ship.y)
-  # This occurs when the left d pad is clicked moving the sprite to the left
-   if keys & ugame.K_LEFT:
-    ship.move(ship.x - 1, ship.y)
-   # This occurs when the up d pad is clicked moving the sprite upwards
-   if keys & ugame.K_UP:
-    ship.move(ship.x, ship.y - 1)
-   # This occurs when the down d pad is clicked moving the sprite downwards
-   if keys & ugame.K_DOWN:
-    ship.move(ship.x, ship.y + 1)
-   
-   #update game logic 
-
-  # Redraw sprite
-   game.render_sprites([ship])
-   game.tick()
-
-
-
+ 
+    #image bank for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprite = stage.Bank.from_bmp16("space_aliens.bmp")
+ 
+    #set the background to image 0 in the image bank
+    #   and the size
+    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+ 
+    #The sprite that will update every frame
+    ship = stage.Sprite(image_bank_sprite, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
+ 
+    #create a stage for the background to show up on
+    #   and set the frame rate to 60 fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    #set the layers of all the sprites, items show up in order
+    game.layers = [ship]+[background]
+    #render all sprites
+    game.render_block()
+ 
+    #repeat forever game loop
+    while True:
+        #get user input
+        keys = ugame.buttons.get_pressed()
+ 
+        #A button to fire
+        if keys & ugame.K_X != 0:
+            if a_button == constants.button_state["button_up"]:
+                a_button = constants.button_state["button_just_pressed"]
+            elif a_button == constants.button_state["button_just_pressed"]:
+                a_button = constants.button_state["button_still_pressed"]
+        else:
+            if a_button == constants.button_state["button_still_pressed"]:
+                a_button = constants.button_state["button_released"]
+            else:
+                a_button = constants.button_state["button_up"]
+        # B button
+        if keys & ugame.K_O:
+            pass
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        #input to make the sprite move
+        if keys & ugame.K_RIGHT:
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+                ship.move(ship.x + 1, ship.y)
+            else:
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
+        if keys & ugame.K_LEFT:
+            if ship.x >= 0:
+                ship.move(ship.x - 1, ship.y)
+            else:
+                ship.move(0, ship.y)
+        if keys & ugame.K_UP:
+            pass
+        if keys & ugame.K_DOWN:
+            pass
+ 
+ 
+        #update game logic
+        #play a sound in A was just pressed
+        if a_button == constants.button_state["button_just_pressed"]:
+            sound.play(pew_sound)
+        #redraw sprite
+        game.render_sprites([ship])
+        game.tick()        
+ 
 if __name__ == "__main__":
- game_scene()
-
+    game_scene()
---
+++ constants.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+# Created by : Frankie Fox
+# Created on  January , 11 , 2023
+# These are the constants in the game xenon which are used for boundaries
+# They also are used for the sprite size , how many aliens
+
+
+# The pybadge screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FP5 = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+
+# Using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released",
+}
